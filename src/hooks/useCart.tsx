c8f5ad635227de420570e4c02f42f3787ae47f4a
Luiz Antonio Gomes da Silva <luiz.gsilva42@gmail.com>
2021-09-14 16:06:44 -0300

    add product to cart

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,4 +1,5 @@
-import { createContext, ReactNode, useContext, useState } from 'react';
+import { createContext, ReactNode, useContext, useEffect, useState } from 'react';
+import { MdAspectRatio } from 'react-icons/md';
 import { toast } from 'react-toastify';
 import { api } from '../services/api';
 import { Product, Stock } from '../types';
@@ -23,20 +24,58 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem('@RocketShoes:cart')
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
 
     return [];
   });
 
+  console.log(cart)
+
+
   const addProduct = async (productId: number) => {
     try {
-      // TODO
+      const response = await api.get(`/stock/${productId}`)
+
+      const stockProduct: Stock = response.data
+      const [ productInCart ] = cart.filter(product => product.id === productId)
+
+      if ( productInCart && stockProduct.amount <= productInCart.amount ) {
+        toast.error('Quantidade solicitada fora de estoque')
+        return
+      }
+
+      if (productInCart) {
+        const newCart = cart.map(item => {
+          if (item.id === productId) {
+            return {
+              ...item,
+              amount: productInCart.amount + 1
+            }
+          }
+          return item
+        })
+
+        setCart(newCart)  
+        localStorage.setItem('@RocketShoes:cart', JSON.stringify(newCart))
+        toast.success('Produto adicionado com sucesso.')
+
+        return
+      }
+
+      const { data } = await api.get(`/products/${productId}`)
+
+      const newCart = [ ...cart, { ...data, amount: 1} ]
+
+      setCart(newCart)  
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(newCart))
+          
+      toast.success('Produto adicionado com sucesso.')
     } catch {
-      // TODO
+      toast.error('Ops... Ocorreu um erro, tente novamente.')
     }
   };
 
